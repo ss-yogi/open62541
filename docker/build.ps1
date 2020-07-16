a23af88ab28332d8b2cc4948e720febd2f335111
Yogi kento <yogi@saltyster.com>
2020-07-16 14:31:15 +0900

    fix path

--- docker/build.ps1
+++ docker/build.ps1
@@ -4,7 +4,7 @@ docker build --tag open62541-build:${Version} -f Dockerfile ..
 docker create open62541-build:${Version}
 $ContainerName=(docker container ls -l --format "{{.Names}}")
 
-docker cp ${ContainerName}:/opt/open62541/build/open62541-${Version}.tar.gz .
+docker cp ${ContainerName}:/opt/open62541/build/open62541.tar.gz ./open62541-${version}-alpine.tar.gz
 docker cp ${ContainerName}:/opt/open62541/pki/created/server_cert.der .
 docker cp ${ContainerName}:/opt/open62541/pki/created/server_key.der .
 
